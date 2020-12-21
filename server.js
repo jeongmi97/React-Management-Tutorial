484240f87218188cdb33b0b9f1b4aed49756f957
kjm7943 <kjm7943@naver.com>
2020-12-21 15:40:33 +0900

    Create Customer File Upload Code

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
 
-const { config } = require('process');
+const multer = require('multer');
+const upload = multer({dest: './upload'})
 
 const connection = mysql.createConnection({
     host: conf.host,
@@ -30,5 +31,22 @@ app.get('/api/customers', (req,res) => {
       }
     );
 });
+
+app.use('/image',express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+  let sql = 'insert into customer values (null, ?, ?, ?, ?, ?)';
+  let image = '/image/' + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  console.log(image);
+  let params = [image, name, birthday, gender, job];
+  connection.query(sql, params,
+    (err, rows, fields) => {
+        res.send(rows);
+    })
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
